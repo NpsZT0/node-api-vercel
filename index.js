f1a241fdd8da1711150eba4c1d8fc696750f173b
NpsZT0 <hearorsimtang@gmail.com>
2023-09-03 01:11:25 +0700

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.listen(PORT, ()=>{
+    console.log(`API listening on PORT ${PORT}`);
+});
+
+app.get('/', (req, res)=>{
+    res.send('This is my API running...');
+});
+
+app.get('/about', (req, res)=>{
+    res.send('This is my about route');
+});
+
+module.export = app;
